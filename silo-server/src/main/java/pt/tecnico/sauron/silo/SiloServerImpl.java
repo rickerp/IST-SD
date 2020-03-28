7bafd7979a1866d034f507a036559ef6f79ed244
Afonso Matos <afonsolfmatos@gmail.com>
2020-03-28 18:30:08 +0000

    Implement cam_join + cam_info

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServerImpl.java
@@ -1,6 +1,7 @@
 package pt.tecnico.sauron.silo;
 
 import io.grpc.stub.StreamObserver;
+import pt.tecnico.sauron.silo.domain.Camera;
 import pt.tecnico.sauron.silo.grpc.*;
 
 public class SiloServerImpl extends SiloGrpc.SiloImplBase {
@@ -17,6 +18,27 @@ public class SiloServerImpl extends SiloGrpc.SiloImplBase {
     @Override
     public void ctrlClear(ClearRequest request, StreamObserver<ClearResponse> responseObserver) {
         ClearResponse response = ClearResponse.newBuilder().build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void camJoin(CamJoinRequest request, StreamObserver<CamJoinResponse> responseObserver) {
+        boolean joined = serverBackend.camJoin(request.getCameraName(), request.getLatitude(), request.getLongitude());
+        CamJoinResponse response = CamJoinResponse.newBuilder()
+                .setSuccess(joined)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void camInfo(CamInfoRequest request, StreamObserver<CamInfoResponse> responseObserver) {
+        Camera camera = serverBackend.getCamera(request.getCameraName());
+        CamInfoResponse response = CamInfoResponse.newBuilder()
+                .setLatitude(camera.latitude)
+                .setLongitude(camera.longitude)
+                .build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
